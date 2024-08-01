62cd6d1dac3197fca78e1066f9bedd87fa1f1985
Shirtsy <48637891+Shirtsy@users.noreply.github.com>
2024-08-01 11:37:45 -0400

    Division support

--- vector2d.lua
+++ vector2d.lua
@@ -1,7 +1,5 @@
-local Vector2D = {
-    x = 0,
-    y = 0,
-}
+local Vector2D = {}
+Vector2D.__index = Vector2D
 
 function Vector2D.new(x, y)
     local vec = {}
@@ -29,10 +27,17 @@ function Vector2D.mul(vec, value)
     return Vector2D.new(x, y)
 end
 
+function Vector2D.div(vec, value)
+    local x = vec.x / value
+    local y = vec.y / value
+    return Vector2D.new(x, y)
+end
+
 Vector2D.metatable = {
     __add = Vector2D.add,
     __sub = Vector2D.sub,
-    __mul = Vector2D.mul
+    __mul = Vector2D.mul,
+    __div = Vector2D.div,
 }
 
 return Vector2D
