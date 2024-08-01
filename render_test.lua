ae9060db1a8042110d00c6cad2076a1a08fa3908
Shirtsy <48637891+Shirtsy@users.noreply.github.com>
2024-08-01 15:45:36 -0400

    Update render_test.lua

--- render_test.lua
+++ render_test.lua
@@ -30,6 +30,9 @@ local function get_pattern(pattern_name)
 end
 
 local pattern_list = {
+    "Flay Mind",
+    "Craft Artifact",
+    {pattern = "aqaawaaqqaa", direction = "SOUTH_EAST"},
     "Mind's Reflection",
     "Compass' Purification II",
     "Gemini Decomposition",
@@ -41,16 +44,28 @@ local pattern_list = {
     "Scout's Distillation",
     "Jester's Gambit",
     "Flay Mind",
-    {pattern = "aqaawaaqqaa", direction = "SOUTH_EAST"}
 }
 
+local function create_pattern_table(pattern_data)
+    local result = {{pattern = "", direction = pattern_data.direction}}  -- Start with an empty string
+    local current = ""
+    for i = 1, #pattern_data.pattern do
+        current = current .. pattern_data.pattern:sub(i,i)
+        table.insert(result, {pattern = current, direction = pattern_data.direction})
+    end
+    return result
+end
+
 term.clear()
-local width, height = 10, 6
+local display_list = create_pattern_table({pattern = "qeqwqwqwqwqeqaeqeaqeqaeqaqded", direction = "NORTH_EAST"})
+local width, height = 50, 18
 local row_length = math.floor(51/width)
-for i, pattern in pairs(pattern_list) do
+for i, pattern in pairs(display_list) do
     local pattern, start_angle = get_pattern(pattern)
     local canv = get_point_canvas(pattern, start_angle, width, height)
     canv:set_foreground_color(colors.yellow)
-    canv:render_canvas((i-1) % row_length * width + 1, 2 + height * math.floor((i-1) / row_length))
+    --canv:render_canvas((i-1) % row_length * width + 1, 2 + height * math.floor((i-1) / row_length))
+    canv:render_canvas(1,2)
+    sleep(0.1)
 end
 term.setCursorPos(1,1)
